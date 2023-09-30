d049bdcdb8b83924638b9d345141b897a71b064a
Feliks Matsko <feliksir@gmail.com>
2023-10-01 01:11:33 +0300

    Add custom actions

---
+++ .github/actions/verify-branch-name/main.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const inputBranch = core.getInput('name');
+    const payload = github.context.payload;
+    const triggeringBranch = payload.ref.replace('refs/heads/', '');
+
+    if (inputBranch === triggeringBranch) {
+        core.info(`Branch name matches: ${inputBranch}`);
+    } else {
+        core.setFailed(`Error: Branch name doesn't match. Expected ${inputBranch}, got ${triggeringBranch}`);
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
