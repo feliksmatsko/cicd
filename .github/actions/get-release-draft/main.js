40d1095e213c7a3b1e57270622b667854f12cf55
Feliks Matsko <feliksir@gmail.com>
2023-10-01 02:54:17 +0300

    Add custom actions

--- .github/actions/get-release-draft/main.js
+++ .github/actions/get-release-draft/main.js
@@ -1,29 +1,27 @@
 const core = require('@actions/core');
-const {request} = require("@octokit/request");
+const github = require('@actions/github');
 
 async function run() {
     try {
         const token = core.getInput('github-token', {required: true});
-        const owner = core.getInput('owner', {required: true});
-        const repo = core.getInput('repo', {required: true});
+        const octokit = github.getOctokit(token);
+        const {owner, repo} = github.context.repo;
 
-        const {data} = await request(`GET /repos/${owner}/${repo}/releases&per_page=3`, {
-            headers: {
-                authorization: `token ${token}`,
-            },
-        });
+        const {data} = await octokit.request(`GET /repos/${owner}/${repo}/releases&per_page=3`);
 
         const tmp = data.map(item => item.name).join(':');
         core.info(`Names list: "${tmp}"`);
 
         const latestDraftRelease = data.find(release => release.draft);
+        console.log(latestDraftRelease)
+        core.info(JSON.stringify(latestDraftRelease));
 
         if (!latestDraftRelease) {
             core.setFailed('No draft releases found.');
             return;
         }
 
-        core.setOutput('latest-draft-release-name', latestDraftRelease.name);
+        core.setOutput('version', latestDraftRelease.name);
     } catch (error) {
         core.setFailed(`Failed to get draft releases: ${error}`);
     }
