9a56c80754c1641360dd22d6e0703d49d8edfc65
Feliks Matsko <feliksir@gmail.com>
2023-10-01 03:40:51 +0300

    Add custom actions

--- .github/actions/get-release-version/main.js
+++ .github/actions/get-release-version/main.js
@@ -4,32 +4,34 @@ const github = require('@actions/github');
 const versionRegExp = /^(v)?\d+\.\d+\.\d+$/;
 
 async function run() {
-    try {
-        const token = core.getInput('github-token', {required: true});
-        const octokit = github.getOctokit(token);
-        const {owner, repo} = github.context.repo;
+  try {
+    const token = core.getInput('github-token', {required: true});
+    const octokit = github.getOctokit(token);
+    const {owner, repo} = github.context.repo;
 
-        const {data} = await octokit.request(`GET /repos/${owner}/${repo}/releases?per_page=5`);
+    const {data} = await octokit.request(
+      `GET /repos/${owner}/${repo}/releases?per_page=5`
+    );
 
-        const latestDraftRelease = data.find(release => release.draft);
+    const latestDraftRelease = data.find(release => release.draft);
 
-        if (!latestDraftRelease) {
-            core.setFailed('No draft releases found.');
-            return;
-        }
-
-        const tagName = latestDraftRelease.tag_name;
-        if (tagName.test(versionRegExp)) {
-            const version = tagName.replace('v', '');
-            core.info(`Release Version: ${version}`);
-            core.setOutput('version', version);
-            await core.summary.addRaw(`Release Version: ${version}`).write();
-        } else {
-            core.setFailed(`Invalid tag name: ${tagName}`);
-        }
-    } catch (error) {
-        core.setFailed(`Failed to get draft releases: ${error}`);
+    if (!latestDraftRelease) {
+      core.setFailed('No draft releases found.');
+      return;
     }
+
+    const tagName = latestDraftRelease.tag_name;
+    if (tagName.test(versionRegExp)) {
+      const version = tagName.replace('v', '');
+      core.info(`Release Version: ${version}`);
+      core.setOutput('version', version);
+      await core.summary.addRaw(`Release Version: ${version}`).write();
+    } else {
+      core.setFailed(`Invalid tag name: ${tagName}`);
+    }
+  } catch (error) {
+    core.setFailed(`Failed to get draft releases: ${error}`);
+  }
 }
 
 run();
