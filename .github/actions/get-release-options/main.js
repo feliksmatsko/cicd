b6c0bcb75de03b185de4e59e63d6782cf8af3a51
Feliks Matsko <feliksir@gmail.com>
2023-10-01 17:25:37 +0300

    Add version

--- .github/actions/get-release-options/main.js
+++ .github/actions/get-release-options/main.js
@@ -1,58 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const versionPattern = /^v\d+\.\d+\.\d+$/;
-let summary = core.summary;
-
-function setOutput(release, propName, displayName) {
-  core.setOutput(propName, release?.tag_name || '');
-
-  if (release) {
-    const message = `${displayName}: ${release.tag_name}`
-    core.info(message);
-    summary = summary.addRaw(`${message}\n`);
-  }
-}
-
-async function run() {
+function run() {
   try {
-    const token = core.getInput('github_token', {required: true});
-    const deriveVersion = core.getInput('derive_version', {required: true});
-    const octokit = github.getOctokit(token);
-    const {owner, repo} = github.context.repo;
-
-    const {data} = await octokit.request(
-      `GET /repos/${owner}/${repo}/releases?per_page=10`
-    );
-
-    const draft = data.find(release => release.draft);
-    const prerelease = data.find(release => release.prerelease);
-    const latest = data.find(release => !release.draft && !release.prerelease);
-
-    setOutput(draft, 'current_tag', 'Current draft');
-    setOutput(prerelease, 'prerelease_tag', 'Release candidate');
-    setOutput(latest, 'latest_tag', 'Latest release');
-
-    await core.summary.write();
-
-    if (deriveVersion === 'false') {
+    const release = github.context.payload.release;
+    if (!release.prerelease) {
+      core.setOutput('prerelease', 'false');
       return;
     }
-
-    if (!draft) {
-      core.setFailed('At least one draft release should be created to continue.');
+    const versionPattern = /^v\d+\.\d+\.\d+$/;
+    if (!versionPattern.test(release.tag_name)) {
+      core.setFailed(`Tag name ${release.tag_name} doesn't match version pattern v0.0.0`);
       return;
     }
-
-    if (!versionPattern.test(draft.tag_name)) {
-      core.setFailed(`Invalid tag name: ${draft.tag_name}`);
-      return;
-    }
-
-    const version = draft.tag_name.replace('v', '');
-    core.setOutput('current_version', version);
+    const version = release.tag_name.replace('v', '');
+    core.setOutput('version', version);
+    core.setOutput('tag_name', release.tag_name);
+    core.setOutput('prerelease', 'true');
   } catch (error) {
-    core.setFailed(`Failed to get draft releases: ${error}`);
+    core.setFailed(error.message);
   }
 }
 
