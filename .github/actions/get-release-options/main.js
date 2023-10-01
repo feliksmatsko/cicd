f9443fd0b4accff6382ecc10a166349f63c963bb
Feliks Matsko <feliksir@gmail.com>
2023-10-01 18:44:38 +0300

    Add version

--- .github/actions/get-release-options/main.js
+++ .github/actions/get-release-options/main.js
@@ -5,7 +5,11 @@ const versionPattern = /^v\d+\.\d+\.\d+$/;
 
 function run() {
   try {
-    const release = github.context.payload.release;
+    const {context} = github;
+    const release = context.payload.release;
+    const repo = context.repo;
+
+    core.info(`Found release tag: ${release.tag_name}`);
     core.setOutput('tag_name', release.tag_name);
 
     if (!versionPattern.test(release.tag_name)) {
@@ -15,6 +19,9 @@ function run() {
 
     const version = release.tag_name.replace('v', '');
     core.setOutput('version', version);
+
+    const url = `https://github.com/${repo.owner}/${repo.repo}/releases/tag/${release.tag_name}`;
+    core.setOutput('url', url);
   } catch (error) {
     core.setFailed(error.message);
   }
