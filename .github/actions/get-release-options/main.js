7255e03c74963b675068f2e93b6072940a1473f5
Feliks Matsko <feliksir@gmail.com>
2023-10-01 18:20:59 +0300

    Add version

--- .github/actions/get-release-options/main.js
+++ .github/actions/get-release-options/main.js
@@ -1,22 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const versionPattern = /^v\d+\.\d+\.\d+$/;
+
 function run() {
   try {
     const release = github.context.payload.release;
-    if (!release.prerelease) {
-      core.setOutput('prerelease', 'false');
-      return;
-    }
-    const versionPattern = /^v\d+\.\d+\.\d+$/;
+    core.setOutput('tag_name', release.tag_name);
+
     if (!versionPattern.test(release.tag_name)) {
       core.setFailed(`Tag name ${release.tag_name} doesn't match version pattern v0.0.0`);
       return;
     }
+
     const version = release.tag_name.replace('v', '');
     core.setOutput('version', version);
-    core.setOutput('tag_name', release.tag_name);
-    core.setOutput('prerelease', 'true');
   } catch (error) {
     core.setFailed(error.message);
   }
