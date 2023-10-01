d87c69a79e97ce2e96fcf99a55d2c639aab65dda
Feliks Matsko <feliksir@gmail.com>
2023-10-01 03:34:22 +0300

    Add custom actions

--- .github/actions/get-release-version/main.js
+++ .github/actions/get-release-version/main.js
@@ -1,29 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const versionRegExp = /^(v)?\d+\.\d+\.\d+$/;
+
 async function run() {
     try {
         const token = core.getInput('github-token', {required: true});
         const octokit = github.getOctokit(token);
         const {owner, repo} = github.context.repo;
 
-        core.info(`123 /repos/${owner}/${repo}/releases?per_page=3`);
-
-        const {data} = await octokit.request(`GET /repos/${owner}/${repo}/releases?per_page=3`);
-
-        const tmp = data.map(item => item.name).join(':');
-        core.info(`Names list: "${tmp}"`);
+        const {data} = await octokit.request(`GET /repos/${owner}/${repo}/releases?per_page=5`);
 
         const latestDraftRelease = data.find(release => release.draft);
-        console.log(latestDraftRelease)
-        core.info(JSON.stringify(latestDraftRelease));
 
         if (!latestDraftRelease) {
             core.setFailed('No draft releases found.');
             return;
         }
 
-        core.setOutput('version', latestDraftRelease.name);
+        const tagName = latestDraftRelease.tag_name;
+        if (tagName.test(versionRegExp)) {
+            const version = tagName.replace('v', '');
+            core.info(`Release Version: ${version}`);
+            core.setOutput('version', version);
+            await core.summary.addRaw(`Release Version: ${version}`).write();
+        } else {
+            core.setFailed(`Invalid tag name: ${tagName}`);
+        }
     } catch (error) {
         core.setFailed(`Failed to get draft releases: ${error}`);
     }
